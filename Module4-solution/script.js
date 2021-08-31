5f8e7f1ff8adf0687723f7fff77c7f41febfa2f9
Megha Bothe <meghabothe@gmail.com>
2021-08-31 22:28:00 +0530

    finish

--- Module4-solution/script.js
+++ Module4-solution/script.js
@@ -1,9 +1,5 @@
-// *******************************
-// START HERE IF YOU WANT A MORE CHALLENGING STARTING POINT FOR THIS ASSIGNMENT
-// *******************************
-//
 // Module 4 Assignment Instructions.
-//
+
 // The idea of this assignment is to take an existing array of names
 // and then output either Hello 'Name' or Good Bye 'Name' to the console.
 // The program should say "Hello" to any name except names that start with a "J"
